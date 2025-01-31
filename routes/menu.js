09ae5692fb93a2bf97b0fe44419762a70a9fbfd6
ridomaulana12 <Rido.Maulana@sera.astra.co.id>
2025-02-01 03:05:06 +0700

    update max upload file config

--- routes/menu.js
+++ routes/menu.js
@@ -5,7 +5,7 @@ const uploadMiddleware = require("../lib/middleware/uploadFile");
 const fs = require("fs");
 
 // ✅ 1. Create a New Menu Item with Image Upload
-router.post("/menu", uploadMiddleware("image", true), async (req, res) => {
+router.post("/menu", uploadMiddleware("images", true), async (req, res) => {
   try {
     const { name, price, description, quantity } = req.body;
     const images = req.files;
@@ -138,7 +138,7 @@ router.get("/menu/:id", async (req, res) => {
 /**
  * ✅ UPDATE MENU - Update a menu item with optional image update
  */
-router.put("/menu/:id", uploadMiddleware("image", true), async (req, res) => {
+router.put("/menu/:id", uploadMiddleware("images", true), async (req, res) => {
   try {
     const menuId = req.params.id;
     const { name, price, description, quantity, deleteImages } = req.body;
